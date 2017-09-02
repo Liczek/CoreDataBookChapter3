83c85a9385d51999a93df325194784245d6bca0f
Paweł Liczmański <liczek@MacBook-Pro-Pawe.local>
2017-09-02 20:44:42 +0200

    entiteies classes created REMEMBER: Codegen: Manual/None  Module: Current Product

---
+++ Chapter 3 - The Core Data Stack/Dog Walk/Dog+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Dog+CoreDataClass.swift
+//  Dog Walk
+//
+//  Created by Paweł Liczmański on 02.09.2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Dog: NSManagedObject {
+
+}
---
+++ Chapter 3 - The Core Data Stack/Dog Walk/Walk+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Walk+CoreDataClass.swift
+//  Dog Walk
+//
+//  Created by Paweł Liczmański on 02.09.2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Walk: NSManagedObject {
+
+}

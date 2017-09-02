e198b8d063c82032132e58dd55d6b3249909bf93
Paweł Liczmański <liczek@MacBook-Pro-Pawe.local>
2017-09-02 21:48:45 +0200

    DELETION CoreData Entitie deleting

--- Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
+++ Chapter 3 - The Core Data Stack/Dog Walk/ViewController.swift
@@ -103,6 +103,33 @@ extension ViewController {
   }
 }
 
+// MARK: Deleting Walks
+
+extension ViewController {
+  
+  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    
+    guard let walkToRemove = currentDog?.walks?[indexPath.row] as? Walk,
+      editingStyle == .delete else {
+      return
+    }
+    
+    managedContext.delete(walkToRemove)
+    
+    do {
+      try managedContext.save()
+      tableView.deleteRows(at: [indexPath], with: .automatic)
+    } catch let error as NSError {
+      print("Removing Walk error: \(error), describtion: \(error.userInfo)")
+    }
+  }
+  
+}
+
 // MARK: UITableViewDataSource
 extension ViewController: UITableViewDataSource {
 

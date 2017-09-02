dd2a596ee4179f6f25d75f46d465bd01b1b48e4d
Paweł Liczmański <liczek@MacBook-Pro-Pawe.local>
2017-09-02 20:03:16 +0200

    Persistent Store + core data stack + managed context

---
+++ Chapter 3 - The Core Data Stack/Dog Walk/CoreDataStack.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataStack.swift
+//  Dog Walk
+//
+//  Created by Paweł Liczmański on 02.09.2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+  
+  private let modelName: String
+  
+  init(modelName: String) {
+    self.modelName = modelName
+  }
+  
+  private lazy var storeContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: self.modelName)
+    container.loadPersistentStores {
+      (storeDescription, error) in
+      if let error = error as NSError? {
+        print("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.storeContainer.viewContext
+  }()
+  
+  func saveContext() {
+    guard managedContext.hasChanges else {return}
+    do {
+      try managedContext.save()
+    } catch let error as NSError {
+      print("Unresolve error \(error), \(error.userInfo)")
+    }
+  }
+  
+}
